1e6eb5106e727ddd2fc5192b70523cfed06618c6
Anurag Pramanik <p.anurag@outlook.com>
2022-04-04 15:15:20 +0530

    add _document.tsx and _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import './globals.css';
+// import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />;
+// }
+
+// export default MyApp;
+
+export default function App({ Component, pageProps }) {
+  console.log('Hello from _app.tsx');
+
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
---
+++ pages/page1.tsx
@@ -0,0 +1,3 @@
+export default function Page1() {
+  return <h1>I am page1</h1>;
+}
